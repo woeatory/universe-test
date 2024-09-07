352dad07aa837ec333bdc127f94536796a193eea
Yurii Bobal <woeatory@gmail.com>
2024-09-08 01:36:20 +0300

    Turbo repo setup

---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,22 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+module.exports = {
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.*.js', 'node_modules/'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
