d44433bfd0457665a304d4de412d61681802ed80
Yurii Bobal <woeatory@gmail.com>
2024-09-11 13:40:59 +0300

    Add apple notification service

--- apps/notifier/src/user/domain/user.service.ts
+++ apps/notifier/src/user/domain/user.service.ts
@@ -1,9 +1,17 @@
+import { Message } from '@aws-sdk/client-sqs';
 import { Injectable, Logger } from '@nestjs/common';
+import { ApnsService } from 'src/apn/apn.service';
 
 @Injectable()
 export class UserService {
   private readonly logger: Logger = new Logger(UserService.name);
-  handle(message) {
+  constructor(private readonly apn: ApnsService) {}
+  handle(messages: Message[]) {
+    const message = messages[0];
     this.logger.log(message);
+    this.apn.sendPushNotification('token', {
+      title: message.MessageId,
+      body: message.Body,
+    });
   }
 }
